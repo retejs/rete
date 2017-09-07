8621cbd2d35f9a810919e3ece1da17628c013262
Vitaliy Stoliarov <huccah@gmail.com>
2017-09-07 19:43:28 +0300

    support of asynchronous workers in the Engine

--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,5 @@
 import {Utils} from './utils';
+var State = { AVALIABLE:0, PROCESSED: 1, ABORT: 2};
 
 export class Engine {
 
@@ -9,49 +10,95 @@ export class Engine {
         
         this.id = id;
         this.worker = worker;
+        this.data = null;
+        this.state = State.AVALIABLE;
+        this.onAbort = function () { };
     }
 
-    process(data, startNode = null) {
+    processStart() {
+        if (this.state === State.AVALIABLE) {  
+            this.state = State.PROCESSED;
+            return true;
+        }
+
+        return false;
+    }
+
+    processDone() {
+        var success = this.state !== State.ABORT;
+
+        this.state = State.AVALIABLE;
         
-        if (!Utils.isValidJSON(data)) throw new Error('Data are damaged'); 
-        if (!Utils.isCompatibleIDs(data.id, this.id)) throw new Error('IDs not compatible');
+        if (!success) {
+            this.onAbort();
+            this.onAbort = function () { }
+        }    
 
-        data = Object.assign({}, data);
-        startNode = startNode || data.nodes[Object.keys(data.nodes)[0]];
+        return success;
+    }
 
-        var backProcess = (node) => {
-            if (node.outputData)
-                return node.outputData;
-            
-            var inputData = node.inputs.map(input => {
-                var connData = input.connections.map(c => {
-                    return backProcess(data.nodes[c.node])[c.output];
-                });
+    async abort() {
+        return new Promise(ret => {
+            if (this.state === State.PROCESSED) {
+                this.state = State.ABORT;
+                this.onAbort = ret;
+            }
+            else
+                ret();
+        });
+    }
 
-                return connData;
-            });
-            var outputData = node.outputs.map(() => null);
-            
-            var key = node.title.toLowerCase();
-
-            node.outputData = this.worker[key](node, inputData, outputData);
-            if (node.outputData.length !== node.outputs.length)
-                throw new Error('Output data does not correspond to number of outputs');
-            
+    async backProcess(node) {
+        if (this.state === State.ABORT)
+            return null;
+        if (node.outputData)
             return node.outputData;
-        }
+        
+        var inputData = await Promise.all(node.inputs.map(async (input) => {
+            var connData = await Promise.all(input.connections.map(async (c) => {
+                return (await this.backProcess(this.data.nodes[c.node]))[c.output];
+            }));
 
-        var forwardProcess = (node) => {
+            return connData;
+        }));
+        
+        node.outputData = node.outputs.map(() => null);
+        
+        var key = node.title.toLowerCase();
 
-            node.outputs.forEach(output => {
-                output.connections.forEach(c => {
-                    backProcess(data.nodes[c.node]);
-                    forwardProcess(data.nodes[c.node]);
-                });
-            });
-        }
+        await this.worker[key](node, inputData, node.outputData);
+        if (node.outputData.length !== node.outputs.length)
+            throw new Error('Output data does not correspond to number of outputs');
+        
+        return node.outputData;
+    }
 
-        backProcess(startNode);
-        forwardProcess(startNode);
+    async forwardProcess(node) {
+        if (this.state === State.ABORT)
+            return null;
+
+        await Promise.all(node.outputs.map(async (output) => {
+            return await Promise.all(output.connections.map(async (c) => {
+                await this.backProcess(this.data.nodes[c.node]);
+                await this.forwardProcess(this.data.nodes[c.node]);
+            }));
+        }));
+    }
+
+    async process(data, startNode = null) {
+        if (!this.processStart()) return 'not started';
+
+        if (!Utils.isValidJSON(data))
+            throw new Error('Data are damaged'); 
+        if (!Utils.isCompatibleIDs(data.id, this.id))
+            throw new Error('IDs not compatible');
+
+        data = this.data = Object.assign({}, data);
+        startNode = startNode || this.data.nodes[Object.keys(data.nodes)[0]];
+        
+        await this.backProcess(startNode);
+        await this.forwardProcess(startNode);
+        
+        return this.processDone()?'success':'aborted';
     }
 }
