b2af7f5c70c89c979089c1d9818f6b50ab53a798
Vitaliy Stoliarov <huccah@gmail.com>
2017-10-27 13:15:24 +0300

    multiple event handlers

--- src/eventlistener.js
+++ src/eventlistener.js
@@ -1,13 +1,25 @@
 export class EventListener {
 
     constructor() {
-        this.events = [];
+        this.events = {
+            nodecreate:[],
+            groupcreate: [],
+            connectioncreate: [],
+            noderemove:[],
+            groupremove:[],
+            connectionremove:[],
+            nodeselect:[],
+            groupselect:[],
+            change:[]
+        };
     }
 
     on(names: string, handler: () => {}) { 
 
         names.split(' ').forEach(name => {
-            this.events[name] = handler;
+            if (!this.events[name])
+                throw new Error(`The event ${name} does not exist`); 
+            this.events[name].push(handler);
         });
         
         return this;
@@ -15,9 +27,11 @@ export class EventListener {
 
     trigger(name: string, args) {
 
-        if (name in this.events)
-            return this.events[name](args) !== false;
-            
-        return true;
+        if (!(name in this.events))
+            throw new Error(`The event ${name} cannot be triggered`);
+        
+        return this.events[name].reduce((r, e) => {
+            return (e(args) !== false) && r
+        }, true); // return false if at least one event is false        
     }
 }
