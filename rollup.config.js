41e5daee69199bb85437d7aeacb5fbaa0c3bd418
Виталий Столяров <huccah@gmail.com>
2017-12-16 17:40:20 +0200

    add banner to bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,13 @@ import babel from 'rollup-plugin-babel';
 import pug from 'rollup-plugin-pug';
 import multiEntry from 'rollup-plugin-multi-entry';
 
+const { name, version } = require('./package.json');
+const banner = `/*!
+* ${name} v${version}
+* (c) 2017 Vitaliy Stoliarov
+* Released under the MIT License.
+*/`;
+
 export default {
     entry: ['src/index.js', 'node_modules/regenerator-runtime/runtime.js'],
     dest: 'build/d3-node-editor.js',
@@ -13,6 +20,7 @@ export default {
         }),
         babel()
     ],
+    banner,
     format: 'umd',
     moduleName: 'D3NE'
 };
