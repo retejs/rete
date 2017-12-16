a880b7ed9554547c893cdb097d7d6e15a807b0f2
Виталий Столяров <huccah@gmail.com>
2017-12-16 22:06:43 +0200

    rollup regenerator

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import pug from 'rollup-plugin-pug';
-import multiEntry from 'rollup-plugin-multi-entry';
 import minify from 'rollup-plugin-minify'
+import regenerator from 'rollup-plugin-regenerator';
 
 const { name, version } = require('./package.json');
 const banner = `/*!
@@ -11,7 +11,7 @@ const banner = `/*!
 */`;
 
 export default {
-    input: ['src/index.js', 'node_modules/regenerator-runtime/runtime.js'],
+    input: 'src/index.js',
     output: {
         file: 'build/d3-node-editor.js',
         sourcemap: true,
@@ -19,11 +19,11 @@ export default {
         name: 'D3NE'
     },
     plugins: [
-        multiEntry(),
         pug({
             pugRuntime: false
         }),
         babel(),
+        regenerator(),
         minify({
             umd: {
                 dest: 'build/d3-node-editor.min.js',
