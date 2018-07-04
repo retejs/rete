59ddf1f2e1a98620b1120cbbfd9ae576c9f6edd6
Vitaliy Stoliarov <huccah@gmail.com>
2018-07-04 17:50:43 +0300

    silent testing

--- test/utils/domReady.js
+++ test/utils/domReady.js
@@ -1,9 +1,11 @@
 import {JSDOM} from 'jsdom';
-const { window } = new JSDOM('<!DOCTYPE html><head></head><body><div id="d3ne"></div></body>');
+const { window } = new JSDOM('<!DOCTYPE html><head></head><body><div id="Rete"></div></body>');
 const { document } = window;
 
 global.document = document;
 global.window = window;
 global.HTMLElement = window.HTMLElement;
 
+console.error = function () {};
+console.warn = function () {};
 global.D3NE = require('../../build/rete');
