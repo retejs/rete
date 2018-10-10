d114ed3330f86c26bd42d36f021dbe3c43dec22b
Vitaliy Stoliarov <huccah@gmail.com>
2018-10-11 01:06:58 +0300

    plugin reuse protection

--- src/core/context.js
+++ src/core/context.js
@@ -10,9 +10,13 @@ export class Context extends Emitter {
             throw new Error('ID should be valid to name@0.1.0 format');  
         
         this.id = id;
+        this.plugins = new Map();
     }
 
     use(plugin, options = {}) {
+        if (plugin.name && this.plugins.has(plugin.name)) throw new Error(`Plugin ${plugin.name} already in use`)
+
         plugin.install(this, options);
+        this.plugins.set(plugin.name, options)
     }
 }
