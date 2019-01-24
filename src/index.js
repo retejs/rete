36ff4116fab95d8cb9cf2ccfc999263778595383
Vitaliy Stoliarov <huccah@gmail.com>
2019-01-25 00:53:36 +0200

    v1.0.2, ES module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import { Component } from './component';
-import { Control } from './control';
-import { Emitter } from './core/emitter';
-import { Engine } from './engine/index';
-import { Input } from './input';
-import { Node } from './node';
-import { NodeEditor } from './editor';
-import { Output } from './output';
-import { Socket } from './socket';
-
-export default {
-    Component,
-    Control,
-    NodeEditor,
-    Emitter,
-    Engine,
-    Input,
-    Node,
-    Output,
-    Socket
-}
+export { Component } from './component';
+export { Control } from './control';
+export { Emitter } from './core/emitter';
+export { Engine } from './engine/index';
+export { Input } from './input';
+export { Node } from './node';
+export { NodeEditor } from './editor';
+export { Output } from './output';
+export { Socket } from './socket';
