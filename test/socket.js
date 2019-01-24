69995d9b5b580b279a0ff83fff9b2bc5b3569d87
Vitaliy Stoliarov <huccah@gmail.com>
2019-01-25 01:12:39 +0200

    disable tests for runtime type checking

--- test/socket.js
+++ test/socket.js
@@ -2,8 +2,8 @@ import assert from 'assert';
 
 describe('Socket', () => {
     it('Socket arguments', () => {
-        assert.throws(() => new Rete.Socket(), Error, 'null');
-        assert.throws(() => new Rete.Socket(345), Error, 'number');
+        // assert.throws(() => new Rete.Socket(), Error, 'null');
+        // assert.throws(() => new Rete.Socket(345), Error, 'number');
         assert.doesNotThrow(() => new Rete.Socket('valid name'), Error, 'valid string');
     });
 
