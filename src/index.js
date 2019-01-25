95468fe11ab53380d5b2cf4cf7719644f8b2cd55
Vitaliy Stoliarov <huccah@gmail.com>
2019-01-25 22:35:47 +0200

    update cli and build cjs, esm and min (exclude Engine bundle)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import { Component } from './component';
+import { Control } from './control';
+import { Emitter } from './core/emitter';
+import { Engine } from './engine/index';
+import { Input } from './input';
+import { Node } from './node';
+import { NodeEditor } from './editor';
+import { Output } from './output';
+import { Socket } from './socket';
+
 export { Component } from './component';
 export { Control } from './control';
 export { Emitter } from './core/emitter';
@@ -7,3 +17,15 @@ export { Node } from './node';
 export { NodeEditor } from './editor';
 export { Output } from './output';
 export { Socket } from './socket';
+
+export default {
+    Component,
+    Control,
+    Emitter,
+    Engine,
+    Input,
+    Node,
+    NodeEditor,
+    Output,
+    Socket
+}
