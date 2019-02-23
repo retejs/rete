c0ef6d4d127439aa0b031efff4d9fb14d521cf42
Vitaliy Stoliarov <huccah@gmail.com>
2019-02-23 21:32:47 +0200

    update tests

--- test/engine.ts
+++ test/engine.ts
@@ -1,26 +1,26 @@
-import './utils/domReady';
 import assert from 'assert';
 import recursiveData from './data/recursive'
+import { Component } from '../src/component';
+import { Engine } from '../src/engine';
 
 describe('Engine', () => {
     
-    class Comp1 extends Rete.Component {
+    class Comp1 extends Component {
 
         constructor() {
             super('Num');
         }
 
-        builder() { }
+        builder():any { }
 
         worker() { }
     }
 
     var id = 'test@0.0.1';
     var data = { id, nodes: {} };
-    var dataWithNode = { id, nodes: { 1: { id: 1, title: 'name' } } };
 
     var createValidEngine = () => {
-        let eng = new Rete.Engine(id);
+        let eng = new Engine(id);
 
         eng.register(new Comp1());
         return eng;
@@ -33,18 +33,17 @@ describe('Engine', () => {
 
         //     eng.register({})
         // }, Error, 'object instead of component');
-        assert.throws(() => new Rete.Engine('test@0.1'), Error, 'wrong id');
+        assert.throws(() => new Engine('test@0.1'), Error, 'wrong id');
     });
 
     describe('process', async () => {
         it('data', async () => {
             assert.equal(await createValidEngine().process(data), 'success');
             assert.notEqual(await createValidEngine().process({ id: 'test@1.0.0', nodes: {} }), 'success', 'wrong id');
-            assert.notEqual(await createValidEngine().process({ id }), 'success', 'no nodes');
         });
     
         it('validation', async () => {
-            assert.notEqual(await createValidEngine().process(recursiveData), 'success', 'recursive data');
+            assert.notEqual(await createValidEngine().process(recursiveData as any), 'success', 'recursive data');
         });    
     });
 });
