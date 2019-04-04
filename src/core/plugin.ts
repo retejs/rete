bdaaedfde9a20b49d3344f245a09bac1e2d04bfe
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-05 02:01:58 +0300

    v1.2.0, static typing of Plugins' params

--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -1,4 +1,6 @@
 export interface Plugin {
     name: string;
-    install: Function;
+    install: (context: any, options: any) => void;
 }
+
+export type PluginParams<T extends Plugin> = T['install'] extends (arg1: any, arg2: infer U) => any ? U : void;
