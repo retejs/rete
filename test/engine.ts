7cfdaa55439e43ba59f7f70eb80aa322b2329e67
Vitaliy Stoliarov <huccah@gmail.com>
2019-05-19 15:24:14 +0300

    [test] engine

--- test/engine.ts
+++ test/engine.ts
@@ -7,33 +7,58 @@ describe('Engine', () => {
     var id = 'test@0.0.1';
     var data = { id, nodes: {} };
 
-    var createValidEngine = () => {
+    function createValidEngine() {
         let eng = new Engine(id);
 
         eng.events['warn'] = [];
         eng.events['error'] = [];
         eng.register(new Comp1());
         return eng;
-    };
+    }
 
     it('init', async () => {
-        assert.doesNotThrow(createValidEngine, Error, 'valid');
+        assert.doesNotThrow(createValidEngine, Error, 'valid')
         // assert.throws(() => {
         //     let eng = createValidEngine(); 
 
         //     eng.register({})
         // }, Error, 'object instead of component');
-        assert.throws(() => new Engine('test@0.1'), Error, 'wrong id');
+        assert.throws(() => new Engine('test@0.1'), Error, 'wrong id')
     });
 
-    describe('process', async () => {
+    describe('instance', async () => {
+        let engine: Engine;
+
+        beforeEach(() => {
+            engine = createValidEngine()
+        })
+
         it('data', async () => {
-            assert.equal(await createValidEngine().process(data), 'success');
-            assert.notEqual(await createValidEngine().process({ id: 'test@1.0.0', nodes: {} }), 'success', 'wrong id');
+            assert.equal(await engine.process(data), 'success')
+            assert.notEqual(await engine.process({ id: 'test@1.0.0', nodes: {} }), 'success', 'wrong id')
         });
     
         it('validation', async () => {
-            assert.notEqual(await createValidEngine().process(recursiveData as any), 'success', 'recursive data');
-        });    
+            assert.notEqual(await engine.process(recursiveData as any), 'success', 'recursive data')
+        });  
+
+        it('clone', () => {
+            const engineClone = engine.clone();
+
+            assert.equal(engineClone instanceof Engine, true, 'is instance')
+            assert.equal(engineClone.id, engine.id, 'id')
+            assert.deepEqual(engineClone.components, engine.components, 'components')
+        })
+
+        it('abort', (done) => {
+            engine.process(data).then(v => {
+                assert.equal(v, 'aborted', 'Check aborted process')
+            }).catch(done)
+            engine.abort();
+            
+            engine.process(data).then(v => {
+                assert.equal(Boolean(v), false, 'Not aborted completely')
+            }).then(done)
+        })
     });
 });
