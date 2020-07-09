595460bbed7c492840ed65e7d22a355c2f5d1b67
Konrad Koch <konrad.koch@pantabio.com>
2020-07-09 21:25:38 +0200

    formatting: prettier

--- src/output.ts
+++ src/output.ts
@@ -5,46 +5,50 @@ import { OutputData } from './core/data';
 import { Socket } from './socket';
 
 export class Output extends IO {
-  
-    constructor(key: string, title: string, socket: Socket, multiConns: boolean = true) {
-        super(key, title, socket, multiConns);
-    }
-    
-    hasConnection() {
-        return this.connections.length > 0;
-    }
+  constructor(
+    key: string,
+    title: string,
+    socket: Socket,
+    multiConns: boolean = true
+  ) {
+    super(key, title, socket, multiConns);
+  }
 
-    connectTo(input: Input) {
-        if (!this.socket.compatibleWith(input.socket))
-            throw new Error('Sockets not compatible');
-        if (!input.multipleConnections && input.hasConnection())
-            throw new Error('Input already has one connection');
-        if (!this.multipleConnections && this.hasConnection())
-            throw new Error('Output already has one connection');
+  hasConnection() {
+    return this.connections.length > 0;
+  }
 
-        const connection = new Connection(this, input);
+  connectTo(input: Input) {
+    if (!this.socket.compatibleWith(input.socket))
+      throw new Error('Sockets not compatible');
+    if (!input.multipleConnections && input.hasConnection())
+      throw new Error('Input already has one connection');
+    if (!this.multipleConnections && this.hasConnection())
+      throw new Error('Output already has one connection');
 
-        this.connections.push(connection);
-        return connection;
-    }
+    const connection = new Connection(this, input);
 
-    connectedTo(input: Input) {
-        return this.connections.some((item) => {
-            return item.input === input;
-        });
-    }
+    this.connections.push(connection);
+    return connection;
+  }
 
-    toJSON(): OutputData {
+  connectedTo(input: Input) {
+    return this.connections.some(item => {
+      return item.input === input;
+    });
+  }
+
+  toJSON(): OutputData {
+    return {
+      connections: this.connections.map(c => {
+        if (!c.input.node) throw new Error('Node not added to Input');
+
         return {
-            'connections': this.connections.map(c => {
-                if (!c.input.node) throw new Error('Node not added to Input');
-
-                return {
-                    node: c.input.node.id,
-                    input: c.input.key,
-                    data: c.data
-                }
-            })
+          node: c.input.node.id,
+          input: c.input.key,
+          data: c.data
         };
-    }
+      })
+    };
+  }
 }
--- src/selected.ts
+++ src/selected.ts
@@ -1,29 +1,26 @@
 import { Node } from './node';
 
 export class Selected {
+  list: Node[] = [];
 
-    list: Node[] = [];
+  add(item: Node, accumulate = false) {
+    if (!accumulate) this.list = [item];
+    else if (!this.contains(item)) this.list.push(item);
+  }
 
-    add(item: Node, accumulate = false) {
-        if (!accumulate)
-            this.list = [item]; 
-        else if (!this.contains(item))
-            this.list.push(item);   
-    }
+  clear() {
+    this.list = [];
+  }
 
-    clear() {
-        this.list = [];
-    }
+  remove(item: Node) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 
-    remove(item: Node) {
-        this.list.splice(this.list.indexOf(item), 1);
-    }
+  contains(item: Node) {
+    return this.list.indexOf(item) !== -1;
+  }
 
-    contains(item: Node) {
-        return this.list.indexOf(item) !== -1;
-    }
-
-    each(callback: (n: Node, index: number) => void) {
-        this.list.forEach(callback);
-    }
+  each(callback: (n: Node, index: number) => void) {
+    this.list.forEach(callback);
+  }
 }
