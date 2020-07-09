11096cc01478e089905689453e81bb01bf661c1a
Konrad Koch <konrad@labk.it>
2020-07-09 21:39:25 +0200

    formatting: prettier and code formatting

--- src/control.ts
+++ src/control.ts
@@ -2,38 +2,42 @@ import { Input } from './input';
 import { Node } from './node';
 
 export class Control {
+  key: string;
+  data: unknown = {};
+  parent: Node | Input | null = null;
 
-    key: string;
-    data: unknown = {};
-    parent: Node | Input | null = null;
-
-    constructor(key: string) {
-        if (this.constructor === Control)
-            throw new TypeError('Can not construct abstract class');
-        if (!key)
-            throw new Error('The key parameter is missing in super() of Control ');
-
-        this.key = key;
+  constructor(key: string) {
+    if (this.constructor === Control) {
+      throw new TypeError('Can not construct abstract class');
+    }
+    if (!key) {
+      throw new Error('The key parameter is missing in super() of Control ');
     }
 
-    getNode() {
-        if (this.parent === null)
-            throw new Error('Control isn\'t added to Node/Input');   
-        
-        if (this.parent instanceof Node)
-            return this.parent;
-            
-        if (!this.parent.node)
-            throw new Error('Control hasn\'t be added to Input or Node');
+    this.key = key;
+  }
 
-        return this.parent.node;
+  getNode() {
+    if (this.parent === null) {
+      throw new Error("Control isn't added to Node/Input");
     }
 
-    getData(key: string) {
-        return this.getNode().data[key];
+    if (this.parent instanceof Node) {
+      return this.parent;
     }
 
-    putData(key: string, data: unknown) {
-        this.getNode().data[key] = data;
-    }  
+    if (!this.parent.node) {
+      throw new Error("Control isn't added to Node/Input");
+    }
+
+    return this.parent.node;
+  }
+
+  getData(key: string) {
+    return this.getNode().data[key];
+  }
+
+  putData(key: string, data: unknown) {
+    this.getNode().data[key] = data;
+  }
 }
