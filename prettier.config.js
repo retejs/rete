b16aa61feb155e228dcacbb9593091355d945b5d
Konrad Koch <konrad@labk.it>
2020-07-09 20:23:50 +0200

    formatting: prettier, imports. Replaced deprecated assert statements

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve'
+};
