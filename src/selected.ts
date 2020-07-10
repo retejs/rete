7193406b41228648e687c470e69336fd498e5eaf
Konrad Koch <konrad.koch@pantabio.com>
2020-07-10 20:14:48 +0200

    documentation: Added documentation

--- src/selected.ts
+++ src/selected.ts
@@ -3,6 +3,12 @@ import { Node } from './node';
 export class Selected {
   list: Node[] = [];
 
+  /**
+   * Adds an item to the list
+   * @param {Node} item
+   * @param {boolean} accumulate defines if the list will be overwritten
+   * with the new item or if it will be added to the list.
+   */
   add(item: Node, accumulate = false) {
     if (!accumulate) this.list = [item];
     else if (!this.contains(item)) this.list.push(item);
