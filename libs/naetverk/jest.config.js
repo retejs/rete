2737585a0632ce0d1e487d21f56ff11a81d5ac55
Konrad Koch <konrad.koch@pantabio.com>
2020-09-14 14:18:09 +0200

    added buildable library

---
+++ libs/naetverk/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'naetverk',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/naetverk',
+};
