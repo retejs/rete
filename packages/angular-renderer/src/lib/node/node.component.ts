4c532a5e4441978cb1dc746c805297a36bc93422
Konrad Koch <konrad@labk.it>
2020-09-14 20:51:40 +0200

    renamed libs to packages and apps to samples

--- packages/angular-renderer/src/lib/node/node.component.ts
+++ packages/angular-renderer/src/lib/node/node.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { NodeEditor, Node, Input as ReteInput, Output as ReteOutput, Control as ReteControl } from 'rete';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnInit,
+} from '@angular/core';
+import {
+  NodeEditor,
+  Node,
+  Input as ReteInput,
+  Output as ReteOutput,
+  Control as ReteControl,
+} from 'rete';
 import { NodeService } from '../node.service';
 
 @Component({
   templateUrl: './node.component.html',
   styleUrls: ['./node.component.sass'],
   providers: [NodeService],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NodeComponent {
+export class NodeComponent implements OnInit {
   @Input() editor!: NodeEditor;
   @Input() node!: Node;
   @Input() bindSocket!: Function;
   @Input() bindControl!: Function;
 
-  constructor(protected service: NodeService, protected cdr: ChangeDetectorRef) {}
+  constructor(
+    protected service: NodeService,
+    protected cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.service.setBindings(this.bindSocket, this.bindControl);
