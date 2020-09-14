42bcd053fbc24ddad5c2e65b8b24c9128388bbd3
Konrad Koch <konrad.koch@pantabio.com>
2020-09-14 14:03:07 +0200

    added angular renderer

---
+++ libs/angular-renderer/src/lib/custom.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit, Injector, ComponentFactoryResolver, ViewContainerRef, Type, ChangeDetectionStrategy } from '@angular/core';
+import { Props } from './types';
+
+@Component({
+    template: '',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CustomComponent implements OnInit {
+  @Input() component!: Type<Component>;
+  @Input() props!: Props;
+
+  constructor(
+    private vcr: ViewContainerRef,
+    private injector: Injector,
+    private factoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit() {
+    const factory = this.factoryResolver.resolveComponentFactory(this.component);
+    const componentRef = factory.create(this.injector);
+    const { props } = this;
+
+    for(let key in props) {
+      Object.defineProperty(componentRef.instance, key, {
+        get() { return props[key]; },
+        set(val) { props[key] = val; }
+      })
+    }
+
+    this.vcr.insert(componentRef.hostView);
+  }
+
+  ngOnDestroy() {
+    this.vcr.detach(0);
+  }
+}
--- libs/naetverk/jest.config.js
+++ libs/naetverk/jest.config.js
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/naetverk',
+  coverageDirectory: '../../coverage/libs/naetverk',
 };
