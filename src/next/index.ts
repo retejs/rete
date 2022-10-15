8146cab4be210415870983a3ea75cfb608e7fa1b
Ni55aN <huccah@gmail.com>
2022-10-16 01:23:15 +0300

    wip

--- src/next/index.ts
+++ src/next/index.ts
@@ -4,8 +4,8 @@ type NodeId = string
 type PortId = string
 type ConnectionId = string
 
-type NodeBase = { id: NodeId }
-type ConnectionBase = { id: ConnectionId, source: NodeId, target: NodeId }
+export type NodeBase = { id: NodeId }
+export type ConnectionBase = { id: ConnectionId, source: NodeId, target: NodeId }
 
 function getUID() {
   const typedArray = new Uint8Array(10)
@@ -65,46 +65,77 @@ export class Connection implements ConnectionBase {
   }
 }
 
-export type Root<NodeData, ConnectionData> =
-  | { type: 'nodecreate', data: NodeData }
-  | { type: 'connectioncreate', data: ConnectionData }
+export type GetSchemes<NodeData, ConnectionData> = { Node: NodeData, Connection: ConnectionData }
+export type BaseSchemes = GetSchemes<NodeBase, ConnectionBase>
+
+export type Root<Scheme extends BaseSchemes> =
+  | { type: 'nodecreate', data: Scheme['Node'] }
+  | { type: 'nodecreated', data: Scheme['Node'] }
+  | { type: 'noderemove', data: Scheme['Node'] }
+  | { type: 'noderemoved', data: Scheme['Node'] }
+  | { type: 'connectioncreate', data: Scheme['Connection'] }
+  | { type: 'connectioncreated', data: Scheme['Connection'] }
+  | { type: 'connectionremove', data: Scheme['Connection']['id'] }
+  | { type: 'connectionremoved', data: Scheme['Connection']['id'] }
+  | { type: 'import' }
+  | { type: 'imported' }
+  | { type: 'export' }
+  | { type: 'exported' }
 
 export * from './scope'
 
-export class NodeEditor<
-  NodeData extends NodeBase = NodeBase,
-  ConnectionData extends ConnectionBase = ConnectionBase
-> extends Scope<Root<NodeData, ConnectionData>> {
-  public nodes: NodeData[] = []
-  public connections: ConnectionData[] = []
+export class NodeEditor<Scheme extends BaseSchemes> extends Scope<Root<Scheme>> {
+  public nodes: Scheme['Node'][] = []
+  public connections: Scheme['Connection'][] = []
 
   constructor(container: HTMLElement) {
     super('NodeEditor')
     container
   }
 
-  addNode(data: NodeData) {
+  async addNode(data: Scheme['Node']) {
+    if (!await this.emit({ type: 'nodecreate', data })) return false
+
     this.nodes.push(data)
+
+    await this.emit({ type: 'nodecreated', data })
+    return true
   }
 
-  addConnection(data: ConnectionData) {
+  async addConnection(data: Scheme['Connection']) {
+    if (!await this.emit({ type: 'connectioncreate', data })) return false
+
     this.connections.push(data)
+
+    await this.emit({ type: 'connectioncreated', data })
+    return true
   }
 
-  removeNode(idOrData: NodeId | NodeData) {
+  async removeNode(idOrData: NodeId | Scheme['Node']) {
     const id = typeof idOrData === 'string' ? idOrData : idOrData.id
     const index = this.nodes.findIndex(n => n.id === id)
 
     if (index < 0) throw new Error('cannot find node')
 
+    if (!await this.emit({ type: 'connectionremove', data: id })) return false
+
     this.nodes.splice(index, 1)
+
+    await this.emit({ type: 'connectionremoved', data: id })
+    return true
   }
 
-  import() {
+  async import() {
+    if (!await this.emit({ type: 'import' })) return false
     1
+    await this.emit({ type: 'imported' })
+    return true
   }
 
-  export() {
+  async export() {
+    if (!await this.emit({ type: 'export' })) return false
     1
+    await this.emit({ type: 'exported' })
+    return false
   }
 }
