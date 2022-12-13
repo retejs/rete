9f01c74de94925e4f2658eca2a3e06a57fc22632
Ni55aN <huccah@gmail.com>
2022-12-13 21:19:26 +0200

    support getUID in node.js

--- src/next/utils.ts
+++ src/next/utils.ts
@@ -1,4 +1,14 @@
+/*global globalThis*/
+
+const nodeCrypto = () => import('node:crypto')
+
+const crypto = globalThis.crypto as (typeof globalThis.crypto | Awaited<ReturnType<typeof nodeCrypto>>)
+
 export function getUID(): string {
+  if ('randomBytes' in crypto) {
+    return crypto.randomBytes(8).toString('hex')
+  }
+
   const bytes = crypto.getRandomValues(new Uint8Array(8))
   const array = Array.from(bytes)
   const hexPairs = array.map(b => b.toString(16).padStart(2, '0'))
