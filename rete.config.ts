18a754a4197a504c2fc0c490c5d8559cbfb03fb2
Ni55aN <huccah@gmail.com>
2023-02-10 21:26:23 +0200

    add global to build config

--- rete.config.ts
+++ rete.config.ts
@@ -2,5 +2,8 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
     input: 'src/index.ts',
-    name: 'Rete'
+    name: 'Rete',
+    globals: {
+        crypto: 'crypto'
+    }
 }
