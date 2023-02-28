6354a11273f35d7fb0eef4f3454d664daa020892
Ni55aN <huccah@gmail.com>
2023-02-28 11:59:45 +0200

    keep postinstall.js

--- rete.config.ts
+++ rete.config.ts
@@ -1,9 +1,17 @@
 import { ReteOptions } from 'rete-cli'
+import copy from 'rollup-plugin-copy'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
   name: 'Rete',
   globals: {
     crypto: 'crypto'
-  }
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'postinstall.js', dest: 'dist' }
+      ]
+    })
+  ]
 }
