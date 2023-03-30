de8dc20c85617ce6f13c759e037d7b4044a8fd0f
ouyanghaozhu <ouyanghaozhu.dk@bytedance.com>
2023-03-30 18:03:49 +0800

    fix: when SocketView.getPosition el.offsetParent is not node.el

--- src/view/socket.ts
+++ src/view/socket.ts
@@ -2,6 +2,7 @@ import { Emitter } from '../core/emitter';
 import { EventsTypes } from '../events';
 import { IO } from '../io';
 import { Node } from '../node';
+import { NodeView } from './node';
 
 export class SocketView extends Emitter<EventsTypes> {
 
@@ -9,13 +10,15 @@ export class SocketView extends Emitter<EventsTypes> {
     type: string;
     io: IO;
     node: Node;
+    nodeView: NodeView;
 
-    constructor(el: HTMLElement, type: string, io: IO, node: Node, emitter: Emitter<EventsTypes>) {
+    constructor(el: HTMLElement, type: string, io: IO, node: Node, emitter: NodeView) {
         super(emitter);
         this.el = el;
         this.type = type;
         this.io = io;
         this.node = node;
+        this.nodeView = emitter
 
         this.trigger('rendersocket', { el, [type]: this.io, socket: io.socket });
     }
@@ -23,9 +26,17 @@ export class SocketView extends Emitter<EventsTypes> {
     getPosition({ position }: { position: number[] }): [number, number] {
         const el = this.el;
 
-        return [
-            position[0] + el.offsetLeft + el.offsetWidth / 2,
-            position[1] + el.offsetTop + el.offsetHeight / 2
-        ]
+        let x = el.offsetLeft + el.offsetWidth / 2;
+        let y = el.offsetTop + el.offsetHeight / 2;
+        let searchDepth = 8;
+        let parent = el.offsetParent! as HTMLElement;
+        while (parent && parent !== this.nodeView.el && searchDepth > 0) {
+            searchDepth--;
+            x += parent.offsetLeft;
+            y += parent.offsetTop;
+            parent = parent.offsetParent as HTMLElement;
+        }
+
+        return [position[0] + x, position[1] + y];
     }
 }
